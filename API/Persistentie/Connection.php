82048fbc0cb3cafa7aeddf07a28789df1860a533
Sam Verschueren <sam.verschueren@gmail.com>
2012-07-06 17:55:00 +0200

    Renaming Connectie to Connection

---
+++ API/Persistentie/Connection.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Connects to database
+ * @author Sam Verschueren
+ */
+class Connectie {
+	
+    const DATABASE_NAME = "db-name";
+    const DATABASE_HOST = "localhost";
+    const DATABASE_USER = "root";
+    const DATABASE_PASSWORD = "";
+    
+    private $db;
+    
+    public function connect() {
+        $this->db = mysql_connect(self::DATABASE_HOST, self::DATABASE_USER, self::DATABASE_PASSWORD);
+        
+        if(!$this->db) {
+        	throw new Exception("Kon geen verbinding maken met de database.");	
+        }
+        
+        mysql_select_db(self::DATABASE_NAME, $this->db);
+    }
+    
+    public function getConnection() {
+        return $this->db;
+    }
+}
+?>
