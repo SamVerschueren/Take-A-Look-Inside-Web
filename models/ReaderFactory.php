049ce88799533652f600c7adc0d178109f7678ab
Sam Verschueren <sam.verschueren@gmail.com>
2012-07-09 14:58:34 +0200

    Documentation added

--- models/ReaderFactory.php
+++ models/ReaderFactory.php
@@ -1,8 +1,19 @@
 <?php
 require_once('SQLReader.php');
 
+/**
+ * Simple factory pattern that creates the reader.
+ * 
+ * @package TakeALookInside/models
+ * @author Sam Verschueren  <sam@irail.be>
+ */
 class ReaderFactory {
     
+    /**
+     * Returns the Reader.
+     * 
+     * @param   type    $type   The type of the reader
+     */
     public static function createReader($type='') {
         $type = trim($type);
         if($type=='') {
