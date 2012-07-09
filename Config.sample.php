2d0a6d6f6eb952c236cea8f7873de63fb20fb464
Sam Verschueren <sam.verschueren@gmail.com>
2012-07-09 14:09:58 +0200

    Added config sample file

---
+++ Config.sample.php
@@ -0,0 +1,9 @@
+<?php
+class Config {
+    // Database variables
+    public static $DB = 'db';
+    public static $DB_HOST = 'localhost';
+    public static $DB_USER = 'user';
+    public static $DB_PASSWORD = 'password';
+}
+?>
