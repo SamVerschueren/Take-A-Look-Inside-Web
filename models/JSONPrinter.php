8509e4147642d94cb1df109b40c4f90f57406860
Lieven Benoot <lievenbenoot@gmail.com>
2012-07-09 16:50:07 +0200

    commit in order to be able to pull

--- models/JSONPrinter.php
+++ models/JSONPrinter.php
@@ -20,14 +20,14 @@ class JSONPrinter implements IPrinter{
         //$link = mysql_connect('localhost','root','root') or die('Cannot connect to the DB');
         //mysql_select_db('TakeALookInside',$link) or die('Cannot select the DB');        
         //$result = mysql_query("select * from Category where categoryID=1");
-        
-        $toPrint=$result;
+         
+        //$toPrint=$result;
         $rows = array();
         while($r = mysql_fetch_assoc($toPrint)) {
             $rows['Category'][] = $r;
            //foreach ($r as $field)
            //     echo $field . "\n";
-        }
+        }        
         print json_encode($rows);
 
          
