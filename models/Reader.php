057ddea5bd35b33bcb5694ce8716499ec7bcc515
Sam Verschueren <sam.verschueren@gmail.com>
2012-07-09 15:01:08 +0200

    Documentation added

--- models/Reader.php
+++ models/Reader.php
@@ -1,6 +1,24 @@
 <?php
+/**
+ * Base class of the Reader.
+ * 
+ * @package TakeALookInside/models
+ * @author Sam Verschueren  <sam@irail.be>
+ */
 interface Reader {
+    
+    /**
+     * Checks whether the parameters are valid.
+     * 
+     * @param parameters    $parameters     The url parameters
+     */
     function isValid($parameters);
+    
+    /**
+     * Executes the SQL string
+     * 
+     * @param sql           $sql            The sql-string that should be executed.
+     */
     function execute($sql);
 }
 ?>
