a963630f29bd03e02c50f3e81e78681d1018061b
Sam Verschueren <sam.verschueren@gmail.com>
2012-07-18 11:57:28 +0200

    Bugfix lowercase category name

--- Site/js/map.js
+++ Site/js/map.js
@@ -181,19 +181,19 @@ function getIcon(buildingID,category){
    if(localStorage["favorites"]!=null){
         $.each(JSON.parse(localStorage["favorites"]), function(key, building) {            
             if(building!=null && building.id==buildingID)
-               icon = 'img/markers/'+category+'[fav].png';     
+               icon = 'img/markers/'+category.toLowerCase()+'[fav].png';     
         });
     }
     if(JSON.parse(localStorage["seen"]!=null)){
         if(icon==null){
             $.each(JSON.parse(localStorage["seen"]), function(key, building) {
                 if(building.id==buildingID)
-                    icon = 'img/markers/'+category+'[seen].png';     
+                    icon = 'img/markers/'+category.toLowerCase()+'[seen].png';     
             });        
         }
     }
     if(icon==null){
-        icon = 'img/markers/'+category+'.png';    
+        icon = 'img/markers/'+category.toLowerCase()+'.png';    
     }     
     return icon;
 }
