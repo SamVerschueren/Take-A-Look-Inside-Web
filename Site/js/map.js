9f7587b25cfd8a4ca252968c3916b965885c4607
Lieven Benoot <lievenbenoot@gmail.com>
2012-07-18 11:04:04 +0200

    minor bugfix

--- Site/js/map.js
+++ Site/js/map.js
@@ -63,8 +63,9 @@ function showMapDirectPopup(){
     if(typeof mapDirect!='undefined'){
         if(markerFeatures[mapDirect].popup==null )
             fillPopup(markerFeatures[mapDirect]);
-        if(!markerFeatures[mapDirect].popup.visible())       
-            showPopup(markerFeatures[mapDirect].popup) 
+        if(markerFeatures[mapDirect].popup !=null && !markerFeatures[mapDirect].popup.visible())       
+            showPopup(markerFeatures[mapDirect].popup)
+        
         mapDirect=undefined;
     }  
 }
