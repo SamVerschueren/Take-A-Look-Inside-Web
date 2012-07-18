1aa21da28f4b85aafead76bafffbeea162dd692d
Sam Verschueren <sam.verschueren@gmail.com>
2012-07-18 17:36:41 +0200

    SQLReader top bug fixed

--- REST/includes/Router.php
+++ REST/includes/Router.php
@@ -32,6 +32,8 @@ class Router {
      * Handles the HTTP Request and binds the url to the right controller. Method based upon TDT project.
      */
     public function processRequest() {
+        header('Access-Control-Allow-Origin: *');
+        
         $method = strtolower($_SERVER['REQUEST_METHOD']);
         $cleanURI = str_replace(Config::$SUBDIR, '', $_SERVER['REQUEST_URI']);
         $cleanURI = trim($cleanURI, '/');
