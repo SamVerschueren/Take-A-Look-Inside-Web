29c44495fba2a144c446436791e9cac355529fef
Lieven Benoot <lievenbenoot@gmail.com>
2012-07-19 17:12:26 +0200

    Added main.js documentation

--- Site/js/main.js
+++ Site/js/main.js
@@ -147,7 +147,7 @@ $(function() {
 });
 
 /**
- * 
+ * Phonegap onDeviceRead()
  */
 function onDeviceReady() {
     deviceUUID=device.uuid;
@@ -156,8 +156,11 @@ function onDeviceReady() {
         // do nothing  
     });      
 }
-
-function checkNetworkState(){
+/**
+ * Checks if the device is connected to the internet
+ * @return      boolean     true if connected
+ */
+function isConnected(){
    return networkState ==
         NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK ||
         NetworkStatus.REACHABLE_VIA_WIFI_NETWORK;
@@ -189,19 +192,21 @@ var scanCode = function() {
     window.plugins.barcodeScanner.scan(function(result) {        
         if(result.text != '') {
             var url = result.text.split('#');
-            
+            //check if QR-code is a TALI QR-code
             if(url[0] != siteUrl) {
                 navigator.notification.alert('You scanned a QR-code that not belongs to Take A Look Inside.', function() { }, 'Wrong QR-code', 'Ok');
                 
                 return;       
             }
-            
+            //get movie
             $.getJSON(server + '/Movie/qrID/' + url[1] + '.json?device=' + deviceUUID, function(data) {                
                 navigator.notification.confirm('The video is ' + $.trim(data.size) + ' KB big. When do you want to see the video?', function(button) {
-                    
+                    //save building in var
                     var building = new Building(data.buildingID, data.buildingName, data.token);
                     
+                    //look later
                     if(button==1) {
+                        //load looklater array from localstorage
                         var array;
                         if(localStorage['lookLater'] == null) {
                             array = new Array();    
@@ -210,23 +215,26 @@ var scanCode = function() {
                             array = JSON.parse(localStorage['lookLater']);
                         }
                         
+                        //push current bulding to looklater array
                         array.push(building);
                         
+                        //store looklater array in localstorage
                         localStorage['lookLater'] = JSON.stringify(array);
                         
                         navigator.notification.alert('The video is saved. You can find it under the Look Later section', function(evt) { }, 'Look Later', 'Ok')
                     }
+                    //look now
                     else if(button==2) {
                         playMovie(building);
                     }
                     
+                    //update looklater screen
                     initHomeContent(false);
                     
                 }, data.buildingName, 'Later,Now');
             });
         }
 
-        // @seealso result.format, result.cancelled
     }, function(error) {
         alert("Could not scan the code. Please try again.");
     });
@@ -243,11 +251,18 @@ function changeContent(goToPage) {
         $('.smallIcon[data-page=' + goToPage + ']').addClass('active');
         
         $('#carrousel').animate({ 'marginLeft' : -1*goToPage*windowWidth });
-        
+        //change content of homepage
         page = goToPage;
     }
 }
 
+/**
+ * Set up home page with different categories
+ * 
+ * @param   boolean     loadMustSee     Determines if the must sees should be loaded again. If it doesn't need to update,
+ * then it improves the performance by not reloading it.
+ * 
+ */
 function initHomeContent(loadMustSee) {
     if(loadMustSee)
         fillHomeCategoryMustSee();
@@ -257,12 +272,19 @@ function initHomeContent(loadMustSee) {
     fillCategory('seen');
 }
 
+/**
+ * Fill must sees in the home screen
+ * Must sees are loaded from the webservice so a different implementation than the fillCategory() should be used.
+ * 
+ */
 function fillHomeCategoryMustSee() {
+    //Clear current content
     $('#mustSee_content').empty();
     
+    //Get JSON of top favorited location from webservice
     $.getJSON('http://tali.irail.be/REST/Building.json?top=3', function(data) {
         var list = $("<ol />"); 
-        
+        //add html elements for each location
         $.each(data.building, function(key, val) {
             var categorySpan = $('<span />').addClass('category').addClass(val.catName.toLowerCase());
             var starSpan = $('<span />').addClass('star').append('<br />').append(val.mustSee);
@@ -274,53 +296,73 @@ function fillHomeCategoryMustSee() {
             var li = $('<li />').attr('id', val.buildingID).append(categorySpan).append(middleSpan).append(starSpan); 
             li.addClass('button');
             list.append(li); 
-            
+            //make html buttons clickabl and link to the map
             li.click(function(event) {
+                //configure mapDirect to the id of the selected location. By doing this,
+                //the map will show the popup of and set center to this building when it is shown.
                 mapDirect = event.target.id;
                 
                 if(mapDirect == '') {
                     mapDirect = $(this).closest("li").attr('id');
                 }
-                
+                //move to Map page
                 window.location.href = "#map";
             });
-        });
-     
+        });     
         list.append("</ol>");
-         
+        
+        //add content to Must See page
         $('#mustSee_content').append(list);
     });
 };
 
+/**
+ * Function which checks if a specific building is in an array. 
+ * 
+ * @param   boolean     result      returns if building is present in the array. 
+ */
 function checkBuildingInArray(array, buildingID){
     var result=false;
-    
+    //loop through all buildings in the array
     $.each(array, function(key, building) {
         if(building!=null)       
             if(building.id==buildingID)
-               result=true;        
+                //found building
+                result=true;        
     });
+    
     return result;
 }
 
+/**
+ * Fills the home categories 'seen', 'looklater' and 'favorites'
+ * All these categories can be filled from the localstorage and use the same implementation.
+ * 
+ */
 function fillCategory(name) {
+    //clear current content
     $('#' + name + '_content').empty();
-        
+    
+    //check if data about the category is present    
     if(localStorage[name] != null) {
         var list = $("<ul />");
-   
+        //loop thourgh array and fill list with locations
         $.each(JSON.parse(localStorage[name]), function(key, building) {
             if(building!=null){
                 var li = $('<li />').attr('id', building.id).html(building.name);
                 li.addClass('button');
-        
+                //add click event to buttons
                 li.click(function(event) {
+                    //if in the favorites manu
                     if(name == 'favorites') {
+                        //set mapdirect to show corresponding popup when redirected to map
                         mapDirect = event.target.id;
-                    
+                        //redirect to the map
                         window.location.href = "#map";    
                     }
-                    else {
+                    //else --> if in the seen or look later menu
+                    else { 
+                        //replay movie
                         playMovie(building);
                     }
                 });
@@ -333,32 +375,61 @@ function fillCategory(name) {
     }
 }
 
+/**
+ * Plays the movie using a native videoplayer.
+ * 
+ */
 function playMovie(building) {    
     $.getJSON(server + '/Device.json?device='+deviceUUID,function(data) {
+        /**
+         * Check if device exists in database
+         * This is a security concern:
+         * 
+         * Users should not be able to watch the movie from outside our application
+         * In order to achieve that, we store a deviceID in our database when our application is launche for the
+         * first time on a specific device.
+         * 
+         * This JSON returns an array with the information whether the device is in the database or not.
+         * 
+         * If the device is not in the database, this means that the url is not requested from our device, 
+         * which means it should not be playable.
+         */
         if(data.exists==true) {
+            //check if the video was added to the looklater
             var seenArray = getLocalStorageArray('seen');
             
             var lookLaterArray = getLocalStorageArray('lookLater');
+            //remove video from looklater array
             lookLaterArray.remove(building);
             
+            //store new looklater array in localstorage
             localStorage['lookLater'] = JSON.stringify(lookLaterArray);
             
+            //check if user already saw current movie
             if(!checkBuildingInArray(seenArray, building.id)) {
-                seenArray.push(building);    
-                
+                //if not, add it to the seen array
+                seenArray.push(building);                    
                 localStorage['seen'] = JSON.stringify(seenArray);
             } 
             
+            //play the movie, using native movieplayer
             window.plugins.videoPlayer.play('http://tali.irail.be/REST/Movie/qrID/' + building.token + '.gp3?device=' + deviceUUID);
        
+            //update home categories except for must sees (these didnt change anyway)    
             initHomeContent(false);
+            
+            //get JSON in order to get category ID of the current building in order to be able to 
+            //update the icon properly (from unseen to seen).
             $.getJSON('http://tali.irail.be/REST/building.json?select=category.name&join=category&buildingID='+building.id, function(data){
+                //update the icon
                 updateIcon(building.id,data.building[0].category)
                 mapDirect = building.id;
                 window.location.href = '#map'; 
             });
 
         }
+        //video not asked from a device stored in the DB: something went wrong, people are trying to get the movie
+        //in ways we would not like them to get it.
         else{
             navigator.notification.alert("Video is only playable from a mobile device.", null, "Device not registered", "OK");;          
         }
