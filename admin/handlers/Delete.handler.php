54b7b326b0afe738d44553cbabe13869be831417
Sam Verschueren <sam.verschueren@gmail.com>
2012-07-20 15:16:11 +0200

    Admin bugfixes

--- admin/handlers/Delete.handler.php
+++ admin/handlers/Delete.handler.php
@@ -8,13 +8,15 @@ if(!isset($_SESSION['ip']) || !isset($_SESSION['user']) || !isset($_SESSION['rol
 require_once('../Connection.php');
 require_once('../Config.php');
 
-$connection = new Connection();
-$connection->connect(Config::$DB_HOST, Config::$DB_USER, Config::$DB_PASSWORD);
-$connection->selectDatabase(Config::$DB);
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $connection = new Connection();
+    $connection->connect(Config::$DB_HOST, Config::$DB_USER, Config::$DB_PASSWORD);
+    $connection->selectDatabase(Config::$DB);
+    
+    $building = $_POST['building'];
+    
+    mysql_query("DELETE FROM building WHERE buildingID='" . mysql_real_escape_string($building) . "'");
+}
 
-$building = $_POST['building'];
-
-mysql_query("DELETE FROM building WHERE buildingID='" . mysql_real_escape_string($building) . "'");
-
-header('Location: ../Home');
+header('Location: ../Home');    
 ?>
--- admin/handlers/Login.handler.php
+++ admin/handlers/Login.handler.php
@@ -8,25 +8,27 @@ if(!isset($_SESSION['ip']) || !isset($_SESSION['user']) || !isset($_SESSION['rol
 require_once('../Connection.php');
 require_once('../Config.php');
 
-$connection = new Connection();
-$connection->connect(Config::$DB_HOST, Config::$DB_USER, Config::$DB_PASSWORD);
-$connection->selectDatabase(Config::$DB);
-
-$userName = $_POST['txtUsername'];
-$password = strtoupper(sha1($_POST['txtPassword']));
-
-$query = mysql_query("SELECT userId, userRole FROM user WHERE userName='" . mysql_real_escape_string($userName) . "' AND userPassword='" . mysql_real_escape_string($password) . "'") or die(mysql_error());
-
-if($query === false) {
-    echo 'Query could not be executed';
-}
-else {    
-    if(mysql_num_rows($query) > 0) {
-        $data = mysql_fetch_assoc($query);
-        
-        $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
-        $_SESSION['user'] = $data['userId'];
-        $_SESSION['role'] = $data['userRole'];
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $connection = new Connection();
+    $connection->connect(Config::$DB_HOST, Config::$DB_USER, Config::$DB_PASSWORD);
+    $connection->selectDatabase(Config::$DB);
+    
+    $userName = $_POST['txtUsername'];
+    $password = strtoupper(sha1($_POST['txtPassword']));
+    
+    $query = mysql_query("SELECT userId, userRole FROM user WHERE userName='" . mysql_real_escape_string($userName) . "' AND userPassword='" . mysql_real_escape_string($password) . "'") or die(mysql_error());
+    
+    if($query === false) {
+        echo 'Query could not be executed';
+    }
+    else {    
+        if(mysql_num_rows($query) > 0) {
+            $data = mysql_fetch_assoc($query);
+            
+            $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+            $_SESSION['user'] = $data['userId'];
+            $_SESSION['role'] = $data['userRole'];
+        }    
     }    
 }
 
