7db23bd0446ce3d469d861f94fd8e76d7024c40d
Lieven Benoot <lievenbenoot@gmail.com>
2012-07-20 10:24:40 +0200

    working on mobile website

---
+++ js/main.js
@@ -0,0 +1,135 @@
+//declare variables
+var homeCategory = "";
+var mapLoaded = false;
+var markerArray = new Array();
+var page = 0;
+
+//Link to the site
+var siteUrl = 'http://tali.irail.be';
+
+//Link to the restfull webservice
+var server = 'http://tali.irail.be/REST'
+
+/**
+ * Extension to arrays:
+ * now possible to use syntax like: some_array.remove(building);
+ */
+Array.prototype.remove = function (building) {
+    for (var i = 0; i < this.length; ) {
+        if (this[i].id === building.id) {
+            this.splice(i, 1);
+        } 
+        else {
+           ++i;
+        }
+    }
+}
+
+/**
+ * JQuery $(document).ready() method
+ * The short way! 
+ */
+$(function() {
+    windowWidth = $(window).width();
+    
+
+    //Testing purposes:
+   // localStorage.clear();
+    
+    /*
+        // Adding some localstorage dummy data
+        var buildings = new Array();
+        buildings[0] = new Building(2, 'Stadhuis');
+        
+        localStorage['favorites'] = JSON.stringify(buildings);
+        
+        
+        var lookLaters = new Array();
+        lookLaters[0] = new Building(3, 'Belfort');
+        localStorage['lookLater'] = JSON.stringify(lookLaters);
+     
+        var seen = new Array();
+        seen[0] = new Building(2, 'Stadhuis');
+        seen[1] = new Building(4, 'Sint-Baafskathedraal'); 
+        localStorage['seen'] = JSON.stringify(seen);
+        */
+    
+
+    
+    /**
+     * Clicking the 'X' (close) button on the information screen.
+     * It sets a value in the localstorage.
+     */
+    /*$('#closeInformation').click(function(event) {
+        localStorage['information'] = 'closed';
+        
+        $('#information, #triangle').fadeOut('slow');    
+    });*/
+    
+
+    
+    /**
+     * Action fired when clicking the 'scan' button 
+     */
+    //$('.scan').click(scanCode);  
+    
+    $('div#fireFilterSection').click(function(event) {
+        
+        $('div#filter').slideToggle('slow', function() {
+            if($('div#filter').is(':visible')) {
+                $('#legendarrow').addClass('rotate');   
+            }
+            else {
+                $('#legendarrow').removeClass('rotate');   
+            }
+        }); 
+    });    
+
+});
+
+/**
+ * Handles the resizing of the window. 
+ */
+$(window).resize(function() {
+    windowWidth = $(window).width();
+});
+
+
+/**
+ * Check's if the building is in the specified array
+ * 
+ * @param   array       The array that should be iterated.
+ * @param   buildingID  The id of the building that should be checked.
+ */
+function checkBuildingInArray(array, buildingID){
+    var result=false;
+    
+    $.each(array, function(key, building) {
+        if(building!=null)       
+            if(building.id==buildingID)
+               result=true;        
+    });
+    return result;
+}
+
+/**
+ * Get the localStorage array
+ *  
+ * @param   string  The key of the array
+ */
+function getLocalStorageArray(string) {
+    var array;
+    
+    /**
+     * If the localStorage array does not exists, create a new array.
+     * If the localStorage array exists, parse the localStorage json string to an array 
+     */
+    if(localStorage[string] == null) {
+        array = new Array();
+    }
+    else {
+        array = JSON.parse(localStorage[string]);
+    }
+    
+    return array;
+}
--- js/map.js
+++ js/map.js
@@ -1,17 +1,48 @@
+var myLat;
+var myLon; 
+var buildingLayer;
+var markerFeatures;
+var activePopup;
+var iconSize = new OpenLayers.Size(25,41);
+var iconOffset = new OpenLayers.Pixel(-(iconSize.w/2), -iconSize.h);
+
 $(function() {
     if(window.location.hash == '') {
-        alert('Go to index page');
+       // alert('Go to index page');
     }
     else {
-        alert('Go to the map, token -> ' + window.location.hash);
+     //   alert('Go to the map, token -> ' + window.location.hash);
     }
     
-    if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(loadMap, function() {
-                alert('Could not detect');
-            });    
-    }         
+ 
 }); 
+$("div#map").live('pageshow', function() {
+    if(!mapLoaded) {
+         if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(loadMap, function() {
+                    alert('Could not detect');
+                });    
+        }  
+    }         
+});
+
+$("div#map").live('pagebeforeshow', function() {  
+    if(mapLoaded){ 
+        if(typeof myRouteVector!='undefined')
+            myRouteVector.destroyFeatures();
+    }else{
+        $.getJSON('http://tali.irail.be/REST/Category.json',function(data){
+            $.each(data.category, function (key,val){
+                var checkbox = $("<input />").attr({type: 'checkbox', id: 'filter' + val.categoryID, checked: 'checked'});
+                $(checkbox).change(filterClick);
+                
+                var label = $("<label />").attr('for', 'filter' + val.categoryID).html(val.name);
+                var li = $("<li />").attr('class', val.name.toLowerCase()).append(checkbox).append(' ').append(label); 
+                $('ul#filterSection').append(li);  
+            });
+        });
+    }              
+});  
 
 function loadMap(position) {
     var mapBoxTiles = new OpenLayers.Layer.XYZ(
@@ -29,7 +60,7 @@ function loadMap(position) {
                                                 numZoomLevels: 18
                                             }
                                          );
-    
+    mapLoaded = true;   
     map = new OpenLayers.Map({
         div: "map",
         theme: null,
